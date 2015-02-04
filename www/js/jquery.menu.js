eeba0832063406b7e8a457fc5b8f1c01f22a329c
gurrumpalad <gurrumpalad@gmail.com>
2015-02-04 18:52:36 +0300

    Stroika
    New excelent view!

--- www/js/jquery.menu.js
+++ www/js/jquery.menu.js
@@ -12,6 +12,9 @@ $(document).ready(function() {
 			menu.css({'top': '0'});
 		}
 	});
+//нажатие на кнопку расчет
+
+
 //фотогалерея
 var slider1 = $('.bxslider').bxSlider({
 		pager: false
@@ -26,6 +29,7 @@ var slider2 = $('.bxslider-main').bxSlider({
 	$(".prev").bind("click",onPrevClick); //обработка нажатия стрелок назад
 	$(".next").bind("click",onNextClick);	// и вперед
 	$(".preview").bind("click",onPreviewClick); //а также переключения изображений по миниатюре
+	$(".projects-description a").bind("click",onRashClick);
 function onPrevClick(){
 	slider2.goToPrevSlide();
 	return false;
@@ -44,3 +48,37 @@ function onPreviewClick(event) {
     return true;
 	}
 });
+function onBackClick(){
+	$("#form-raschet").css({'display':'none'});
+	return false;
+}
+function onRashClick(){
+	var width = document.documentElement.clientWidth;
+	var height = document.documentElement.clientHeight;
+	$("#form-raschet").css({'width' : width,'height' : height,'position' : 'fixed','top' : '0','left' : '0','right' : width,'bottom' : height}).fadeTo(150,1,function(){
+		$(this).css({'display':'block'});
+	});
+	$("#form-raschet").bind("dblclick",function(){
+		$(this).fadeTo(300,0,function(){
+			$(this).css({'display' : 'none'});
+		});
+	});
+	return false;
+}
+function onRashSubmit() {
+	var square = $('#square').val();
+	var floor = $('#floor').val();
+	var check = $('#mans').is(":checked");
+	var sum = square*14000+floor*50000;
+	if(check) {
+		sum = sum + 100000;
+	}
+	$("#danie").fadeTo(300,0,function(){
+		$(this).css({'display':'none'});
+		$("#rez").fadeTo(150,1,function(){
+		$(this).css({'display':'block'});
+		});
+	});
+	alert("Примерная стоимость постройки Вашего дома: "+sum+" руб.");
+	return false;
+}
