aeab971ae326c9ed452fd2eeeb1613be94e5295b
gurrumpalad <gurrumpalad@gmail.com>
2015-02-07 19:33:38 +0300

    Last summary
    Hi! This is last corrections in the site karkas.

--- www/js/jquery.menu.js
+++ www/js/jquery.menu.js
@@ -29,30 +29,36 @@ var slider2 = $('.bxslider-main').bxSlider({
 	$(".prev").bind("click",onPrevClick); //обработка нажатия стрелок назад
 	$(".next").bind("click",onNextClick);	// и вперед
 	$(".preview").bind("click",onPreviewClick); //а также переключения изображений по миниатюре
-	$(".projects-description a").bind("click",onRashClick);
-function onPrevClick(){
+	$(".projects-description a").bind("click",onZakClick);
+	
+	
+function onPrevClick(){ //проекты клик назад
 	slider2.goToPrevSlide();
+	$(".projects-description").css({'display':'block'});
 	return false;
 }
-function onNextClick(){
+function onNextClick(){ //проекты клик вперед
 	slider2.goToNextSlide();
+	$(".projects-description").css({'display':'block'});
 	return false;
 }
-function onPreviewClick(event) {
+function onPreviewClick(event) { //обработка нажатия превью в Проектах
 	$(".bigview").fadeTo(30,0.33,function(){
 		$($(event.currentTarget).parent().parent().children('.bigview').html($(event.currentTarget).html()));
 		$(this).fadeTo(200,1);
+		$($(event.currentTarget).parent().parent().children(".projects-description").css({'display':'none'}));
 	});
 	$(".preview").add(".bigview").css("background","inherit");
 	$(event.currentTarget).css("background-color","#a6a6ff");
-    return true;
+	//setTimeout(function () {$($(event.currentTarget).parent().parent().children(".projects-description").css({'display':'block'}));},4000);
+    return false;
 	}
 });
-function onBackClick(){
+function onBackClick(){ //обработка нажатия кнопки "назад" в формах
 	$("#form-raschet").css({'display':'none'});
 	return false;
 }
-function onRashClick(){
+function onRashClick(){ //расчет
 	var width = document.documentElement.clientWidth;
 	var height = document.documentElement.clientHeight;
 	$("#form-raschet").css({'width' : width,'height' : height,'position' : 'fixed','top' : '0','left' : '0','right' : width,'bottom' : height}).fadeTo(150,1,function(){
@@ -65,7 +71,7 @@ function onRashClick(){
 	});
 	return false;
 }
-function onRashSubmit() {
+function onRashSubmit() { //расчет
 	var square = $('#square').val();
 	var floor = $('#floor').val();
 	var check = $('#mans').is(":checked");
@@ -80,5 +86,22 @@ function onRashSubmit() {
 		});
 	});
 	alert("Примерная стоимость постройки Вашего дома: "+sum+" руб.");
+	return false;
+}
+function onZakClick(){ //заказ
+	var width = document.documentElement.clientWidth;
+	var height = document.documentElement.clientHeight;
+	$("#form-zakaz").css({'width' : width,'height' : height,'position' : 'fixed','top' : '0','left' : '0','right' : width,'bottom' : height}).fadeTo(150,1,function(){
+		$(this).css({'display':'block'});
+	});
+	$("#form-zakaz").bind("dblclick",function(){
+		$(this).fadeTo(300,0,function(){
+			$(this).css({'display' : 'none'});
+		});
+	});
+	return false;
+}
+function onBackClick2(){ //обработка нажатия кнопки "назад" в формах
+	$("#form-zakaz").css({'display':'none'});
 	return false;
 }
